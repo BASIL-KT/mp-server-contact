ed0c7598b14d194d1ef97ed4a89303dc67890c3c
basilkt <basilkt2001@gmail.com>
2024-10-11 00:40:01 +0530

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+const jsonServer=require('json-server')
+
+const mpServer=jsonServer.create()
+const middleware=jsonServer.defaults()
+const routes=jsonServer.router('db.json')
+
+
+mpServer.use(middleware)
+mpServer.use(routes)
+
+const PORT=3000 || process.env.PORT
+
+mpServer.listen(PORT,()=>{
+    console.log('Server running at:'+PORT)
+})
